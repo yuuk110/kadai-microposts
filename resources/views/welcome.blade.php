270cb574bdc6ca89394f88f13cf437c3c19b6620
yuuki.suzuki <yuuki_suzuki@tglab.jp>
2022-08-29 05:33:23 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+  @if (Auth::check())
+     {{ Auth::user()->name }}
+  @else
    <div class="center jumbotron">
        <div class="text-center">
            <h1>Welcome to the Microposts</h1>
@@ -8,4 +11,5 @@
            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
        </div>
    </div>
+   @endif
 @endsection
