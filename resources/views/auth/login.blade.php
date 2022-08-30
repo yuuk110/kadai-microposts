0c391f952377df01798c39a0d205b0055827f087
yuuki.suzuki <yuuki_suzuki@tglab.jp>
2022-08-30 08:34:40 +0000

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
            {!! Form::open(['route' => 'login.post']) !!}
                <div class="form-group">
                    {!! Form::label('email', 'Email') !!}
-                   {!! Form::email('email', null, ['class' => form-control]) !!}
+                   {!! Form::email('email', null, ['class' => 'form-control']) !!}
                </div>
                
                <div class="form-group">
