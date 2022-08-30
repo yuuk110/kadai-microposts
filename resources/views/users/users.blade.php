bf2612ac36ffb9f6602dd7020a21fde98ba682d2
yuuki.suzuki <yuuki_suzuki@tglab.jp>
2022-08-30 01:55:34 +0000

    user pages

---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,21 @@
+@if (count($users) > 0)
+   <ul class="list-unstled">
+      @foreach ($users as $user)
+         <li class="media">
+             {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+             <img class="mr-2 rounded" src="{{ Gravatar::get($user->email, ['size' => 50]) }}" alt="">
+             <div class="media-body">
+                 <div>
+                     {{ $user->name }}
+                 </div>
+                 <div>
+                     {{-- ユーザ詳細ページへのリンク --}}
+                     <P>{!! link_to_route('users.show', 'View profile', ['user' => $user->id]) !!}</P>
+                 </div>
+             </div>
+         </li>
+        @endforeach 
+   </ul>
+   {{-- $users->links() --}}
+
+@endif
